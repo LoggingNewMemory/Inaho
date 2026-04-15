2ef83ceda102fb6b59f8bbc2d4982a02575a0fa8
KanagawaYamada <albert.wesley.dion@gmail.com>
2026-04-15 07:51:36 +0700

    Added program header and desc in license
    Signed-off-by: KanagawaYamada <albert.wesley.dion@gmail.com>

--- AppSource/app/src/main/java/com/kanagawa/yamada/inaho/YamadaEQ.kt
+++ AppSource/app/src/main/java/com/kanagawa/yamada/inaho/YamadaEQ.kt
@@ -1,4 +1,5 @@
 /*
+Inaho Music Player - A Music Player that inspired with Ochinai Inaho
 Copyright (C) 2026 Kanagawa Yamada 
 This program is free software: you can redistribute it and/or modify it under the terms of 
 the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. 
