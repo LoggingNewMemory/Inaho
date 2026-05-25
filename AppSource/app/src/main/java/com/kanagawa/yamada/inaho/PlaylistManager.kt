7408d694804522b81361250f0faf0043f1b0fb35
KanagawaYamada <albert.wesley.dion@gmail.com>
2026-05-25 08:18:00 +0700

    Shorten Copyright notice

--- AppSource/app/src/main/java/com/kanagawa/yamada/inaho/PlaylistManager.kt
+++ AppSource/app/src/main/java/com/kanagawa/yamada/inaho/PlaylistManager.kt
@@ -1,15 +1,6 @@
 /*
-Inaho Music Player - A Music Player that inspired with Ochinai Inaho
-Copyright (C) 2026 Kanagawa Yamada 
-This program is free software: you can redistribute it and/or modify it under the terms of 
-the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. 
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
-See the GNU General Public License for more details. 
-You should have received a copy of the GNU General Public License along with this program. 
-
-If not, see https://www.gnu.org/licenses/.
+Inaho Music Player - Inaho Music Player
+Copyright (C) 2026 Kanagawa Yamada
 */
 
 package com.kanagawa.yamada.inaho
